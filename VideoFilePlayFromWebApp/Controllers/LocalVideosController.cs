99486c458b9724cedda85a6ff986a8d20fca3566
Gowtham <Gowthambe91@gmail.com>
2019-11-04 09:20:41 +0530

    VideoFilePlayFromWebApp - "iframe" initial commit

---
+++ VideoFilePlayFromWebApp/Controllers/LocalVideosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VideoFilePlayFromWebApp.Controllers
+{
+    public class LocalVideosController : Controller
+    {
+        // GET: LocalVideos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
