6ad0a74ca581931a0022b767a8675b5e11f9bb78
Gowtham <Gowthambe91@gmail.com>
2019-11-06 00:12:43 +0530

    UploadFile Added - https://www.c-sharpcorner.com/article/asp-net-mvc-form-with-file-upload/

---
+++ VideoFilePlayFromWebApp/Controllers/ContactFormController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoFilePlayFromWebApp.Models;
+
+
+namespace VideoFilePlayFromWebApp.Controllers
+{
+    public class ContactFormController : Controller
+    {
+        // GET: ContactForm
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(MemberModel model)
+        {
+            string FileName = Path.GetFileNameWithoutExtension(model.ImageFile.FileName);
+            string FileExtension = Path.GetExtension(model.ImageFile.FileName);
+
+            FileName = DateTime.Now.ToString("yyyyMMdd") + "-" + FileName.Trim() + FileExtension;
+
+            string UploadPath = ConfigurationManager.AppSettings["UserUploadPath"].ToString();
+
+            model.ImagePath = UploadPath + FileName;
+
+            model.ImageFile.SaveAs(model.ImagePath);
+
+            tblMember member = new tblMember();
+            
+            member.ImagePath = model.ImagePath;
+            member.MemberName = model.Name;
+            member.PhoneNumber = model.PhoneNumber;
+
+            using (MVCEntities entities = new MVCEntities())
+            {             
+
+                entities.tblMembers.Add(member);
+                entities.SaveChanges();
+            }
+
+                return View();
+        }
+    }
+}
---
+++ VideoFilePlayFromWebApp/Models/MemberModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace VideoFilePlayFromWebApp.Models
+{
+    public class MemberModel
+    {
+        [Display(Name = "Member Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Telephone/Mobile No.")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Upload File")]
+        public string ImagePath { get; set; }
+                
+        public HttpPostedFileBase ImageFile { get; set; }
+    }
+}
